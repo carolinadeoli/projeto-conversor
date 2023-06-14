664e0ad8b30c86418760918e06e088d2f40d71e4
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-06-14 18:36:19 -0300

    colocando código

--- conversor.js
+++ conversor.js
@@ -1,3 +1,9 @@
 function Converter(){
-    
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmReal = parseFloat(valor);
+    var valorEmDolar = valorEmReal / 4.80; //valor do dólar em 14 de junho de 2023
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "O resultado em dolar é: U$ " + valorEmDolar;
+    elementoValorConvertido.innerHTML = valorConvertido;
 }
